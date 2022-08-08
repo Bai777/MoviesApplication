f0389b058bd04f52143574cd2f50e66376439cc8
bai_777 <bai_777@mail.ru>
2022-08-08 20:36:55 +0300

    добавил классы Film.kt, MainRecyclerAdapter.kt, MainViewModel.kt

--- app/src/main/java/com/example/moviesapplication/ui/MainScreenFragment.kt
+++ app/src/main/java/com/example/moviesapplication/ui/MainScreenFragment.kt
@@ -1,20 +1,82 @@
 package com.example.moviesapplication.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.moviesapplication.R
+import com.example.moviesapplication.databinding.MainScreenLayoutBinding
+import com.example.moviesapplication.domain.Film
 
 
 class MainScreenFragment : Fragment() {
+
+    private var _binding: MainScreenLayoutBinding? = null
+    private val binding
+        get() = _binding!!
+
+    private val viewModel: MainViewModel by lazy {
+        ViewModelProvider(this).get(MainViewModel::class.java)
+    }
+    private val adapter = MainRecyclerAdapter()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.main_screen_layout, container, false)
+        _binding = MainScreenLayoutBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        adapter.setOnItemViewClickListener(object : OnItemViewClickListener {
+            override fun onItemViewClick(filmId: Film) {
+                activity?.supportFragmentManager?.apply {
+                    beginTransaction()
+                        .replace(R.id.container, DetailsFragment.newInstance(Bundle().apply {
+                            putParcelable(DetailsFragment.BUNDLE_EXTRA, filmId)
+                        }))
+                        .addToBackStack("")
+                        .commitAllowingStateLoss()
+                }
+            }
+        })
+
+        binding.mainFragmentRecyclerView.layoutManager = GridLayoutManager(requireContext(), 2)
+        binding.mainFragmentRecyclerView.adapter = adapter
+
+        val observer = Observer<MainAppState> {
+            renderData(it)
+        }
+        viewModel.getData().observe(viewLifecycleOwner, observer)
+        viewModel.requestFilms()
+    }
+
+    private fun renderData(data: MainAppState) = when (data) {
+        is MainAppState.Success -> {
+            val dataList = data.dataList
+            binding.loadingLayout.visibility = View.GONE
+            adapter.setData(dataList)
+        }
+        is MainAppState.Loading -> {
+            binding.loadingLayout.visibility = View.VISIBLE
+        }
+        is MainAppState.Error -> {
+            binding.loadingLayout.visibility = View.GONE
+        }
+    }
+
+    interface OnItemViewClickListener {
+        fun onItemViewClick(filmId: Film)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
---
+++ app/src/main/java/com/example/moviesapplication/ui/MainViewModel.kt
@@ -0,0 +1,36 @@
+package com.example.moviesapplication.ui
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.schedulers.Schedulers
+
+class MainViewModel(private val repo: Repository = RemoteRepositoryImpl(Retrofit().getService())) :
+    ViewModel() {
+
+    private val liveDataToObserve: MutableLiveData<MainAppState> = MutableLiveData()
+    fun getData(): LiveData<MainAppState> {
+        return liveDataToObserve
+    }
+
+    fun requestFilms() = requestFilmsFromApi()
+
+    private fun requestFilmsFromApi() {
+        repo.getFilms()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .doOnSubscribe {
+                liveDataToObserve.postValue(MainAppState.Loading)
+            }
+            .subscribe({
+                liveDataToObserve.postValue(MainAppState.Success(it))
+            }, {
+                liveDataToObserve.postValue(MainAppState.Error(Throwable(ERROR_MESSAGE)))
+            })
+    }
+
+    companion object {
+        const val ERROR_MESSAGE = "RxJava Error"
+    }
+}
