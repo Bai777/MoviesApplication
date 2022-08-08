0e3ebd076d4b284d11dd6df66f1c3c8c403df5ce
bai_777 <bai_777@mail.ru>
2022-08-08 19:22:05 +0300

    добавил зависимости, разбил приложение на слои

--- app/src/main/java/com/example/moviesapplication/ui/MainActivity.kt
+++ app/src/main/java/com/example/moviesapplication/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.moviesapplication
+package com.example.moviesapplication.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.moviesapplication.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
