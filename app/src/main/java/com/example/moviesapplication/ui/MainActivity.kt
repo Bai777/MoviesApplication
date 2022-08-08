f24ffd7354af5ce4c06afcff1287c21925f291fe
bai_777 <bai_777@mail.ru>
2022-08-08 19:44:45 +0300

    добавил MainScreenFragment.kt, default_background.jpg, default_poster.jpg, cast_recycler_item.xml, main_recycler_item.xml, main_screen_layout.xml, styles.xml

--- app/src/main/java/com/example/moviesapplication/ui/MainActivity.kt
+++ app/src/main/java/com/example/moviesapplication/ui/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.moviesapplication.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.moviesapplication.R
+import com.example.moviesapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (savedInstanceState == null)
+            supportFragmentManager.beginTransaction().replace(R.id.container, MainScreenFragment())
+                .commitNow()
     }
 }
---
+++ app/src/main/java/com/example/moviesapplication/ui/MainScreenFragment.kt
@@ -0,0 +1,20 @@
+package com.example.moviesapplication.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.moviesapplication.R
+
+
+class MainScreenFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.main_screen_layout, container, false)
+    }
+}
